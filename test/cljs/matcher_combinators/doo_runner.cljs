5cb855b4e705413af40c0dafae7a082f8d95f7c2
Marco Biscaro <marco.biscaro@nubank.com.br>
2023-01-19 19:29:08 -0300

    Run all test namespaces in cljs

--- test/cljs/matcher_combinators/doo_runner.cljs
+++ test/cljs/matcher_combinators/doo_runner.cljs
@@ -1,7 +1,19 @@
 (ns matcher-combinators.doo-runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            matcher-combinators.cljs-example-test))
+            [matcher-combinators.cljs-example-test]
+            [matcher-combinators.core-test]
+            [matcher-combinators.matchers-test]
+            [matcher-combinators.parser-test]
+            [matcher-combinators.printer-test]
+            [matcher-combinators.standalone-test]
+            [matcher-combinators.test-test]))
 
 (enable-console-print!)
 
-(doo-tests 'matcher-combinators.cljs-example-test)
+(doo-tests 'matcher-combinators.cljs-example-test
+           'matcher-combinators.core-test
+           'matcher-combinators.matchers-test
+           'matcher-combinators.parser-test
+           'matcher-combinators.printer-test
+           'matcher-combinators.standalone-test
+           'matcher-combinators.test-test)
